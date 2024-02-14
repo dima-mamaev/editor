5bd29d94221bf17687a79bdaae0771bdb4b7b7fc
dima-mamaev <dmitriy.mam@2muchcoffee.com>
2024-02-14 13:09:07 +0200

    build: preparations before adding platejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
 
 function App() {
+  const sections = [
+    {
+      id: 'section-1',
+      title: 'Section 1',
+      blocks: [
+        {
+          id: '1123123-12312_czc',
+          type: 'p',
+          children: [{ text: 'Hello, World!' }],
+        },
+      ]
+    },
+    {
+      id: 'section-2',
+      title: 'Section 2',
+      blocks: [
+        {
+          id: '1123123-12312_czc',
+          type: 'p',
+          children: [{ text: 'Hello, World!' }],
+        },
+      ]
+    }
+  ]
   return (
-    <div>Hello world</div>
+    <section>
+      <div>PlateJS Editor</div>
+      {
+        sections.map(section => {
+          return (
+            <div>{section.id}</div>
+          )
+        })
+
+      }
+    </section>
   )
 }
 
