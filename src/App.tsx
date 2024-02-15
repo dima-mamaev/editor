a8f4ccf31ff9fffaa6de218972564dc49852ea6f
dima-mamaev <dmitriy.mam@2muchcoffee.com>
2024-02-15 10:25:57 +0200

    refactor: ui adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './styles/globals.css'
 function App() {
   const sections = [
     {
-      id: 'section-1',
+      id: 'section-1_asdad3214-24234',
       title: 'Section 1',
       blocks: [
         {
@@ -15,11 +15,22 @@ function App() {
         },
       ]
     },
+    {
+      id: 'section-2_oaosdoas0343434_adad',
+      title: 'Section 2',
+      blocks: [
+        {
+          id: '1123123-123asdas12_czc',
+          type: 'p',
+          children: [{ text: 'Hello, World! Section2' }],
+        },
+      ]
+    },
   ]
   return (
-    <section>
+    <section style={{ backgroundColor: 'antiquewhite', minHeight: "100vh" }}>
       <div>PlateJS Editor</div>
-      <section style={{ margin: "50px" }}>
+      <section style={{ margin: "90px" }}>
         {
           sections.map(section => {
             return (
--- src/components/plate-ui/editor.tsx
+++ src/components/plate-ui/editor.tsx
@@ -9,36 +9,16 @@ import type { VariantProps } from 'class-variance-authority';
 const editorVariants = cva(
   cn(
     'relative whitespace-pre-wrap break-words',
-    'min-h-[80px] w-full rounded-md bg-white px-5 py-5 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400',
+    'min-h-[300px] w-full rounded-md bg-white px-[4.5rem] py-5 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400',
     '[&_[data-slate-placeholder]]:text-slate-500 [&_[data-slate-placeholder]]:!opacity-100 dark:[&_[data-slate-placeholder]]:text-slate-400',
     '[&_[data-slate-placeholder]]:top-[auto_!important]',
     '[&_strong]:font-bold'
   ),
   {
     variants: {
-      variant: {
-        outline: 'border border-slate-200 dark:border-slate-800',
-        ghost: '',
-      },
-      focused: {
-        true: 'ring-2 ring-slate-950 ring-offset-2 dark:ring-slate-300',
-      },
       disabled: {
         true: 'cursor-not-allowed opacity-50',
       },
-      focusRing: {
-        true: 'focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:focus-visible:ring-slate-300',
-        false: '',
-      },
-      size: {
-        sm: 'text-sm',
-        md: 'text-base',
-      },
-    },
-    defaultVariants: {
-      variant: 'outline',
-      focusRing: true,
-      size: 'sm',
     },
   }
 );
@@ -51,11 +31,8 @@ const Editor = React.forwardRef<HTMLDivElement, EditorProps>(
     {
       className,
       disabled,
-      focused,
-      focusRing,
       readOnly,
-      size,
-      variant,
+
       ...props
     },
     ref
@@ -66,10 +43,7 @@ const Editor = React.forwardRef<HTMLDivElement, EditorProps>(
           className={cn(
             editorVariants({
               disabled,
-              focused,
-              focusRing,
-              size,
-              variant,
+
             }),
             className
           )}
